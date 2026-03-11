eb45ecc00f1c73aed604ad408c71ec577a71eece
SmXThing <aogletree2005@gmail.com>
2026-03-11 08:24:50 -0500

    Added Base of State Machine

--- scripts/torch.gd
+++ scripts/torch.gd
@@ -1,4 +1,13 @@
 extends AnimatedSprite2D
 
+var it: int = 0
+
+@onready var light: PointLight2D = $Light
+
 func _ready() -> void:
 	play("default")
+
+func _on_flicker_timer_timeout() -> void:
+	var rad: float = PI/2 * it
+	light.energy = 1 + 0.1 * sin(rad)
+	it += 1
