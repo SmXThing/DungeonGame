1d7da2443638596293c11f44d3b3c312dcfc7bab
SmXThing <aogletree2005@gmail.com>
2026-03-19 10:48:32 -0500

    Added rudimentary state machine, allowing for switching between idle and walking for now, which can trigger either animations. Responds to direction as well. Also added sprint (Activated by pressing SHIFT)

--- scripts/idle.gd
+++ scripts/idle.gd
@@ -1,4 +1,23 @@
 extends State
 
-func _input(event: InputEvent) -> void:
+func update(_delta: float) -> void:
+	if Input.is_action_just_pressed("KEY_W") || Input.is_action_just_pressed("KEY_A") || Input.is_action_just_pressed("KEY_S") || Input.is_action_just_pressed("KEY_D"):
+		emit("walking")
+	'''if Input.is_action_just_pressed("ENTER") || Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		emit("attacking")'''
+
+func physics_update(_delta: float) -> void:
+	pass
+
+func enter() -> void:
+	if get_parent().get_last_direction().y == -1:
+		animation_sprite.play("idle_up")
+	else:
+		animation_sprite.play("idle_down") 
+	if get_parent().get_last_direction().x == 1:
+		animation_sprite.flip_h = false
+	else:
+		animation_sprite.flip_h = true
+
+func exit() -> void:
 	pass
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -23,6 +23,7 @@ var armor: Armor
 
 var status: Array
 var time: float = 0
+var facing: Vector2 = Vector2(1, 1)
 
 func _ready() -> void:
 	if enable_camera_limit:
@@ -33,25 +34,18 @@ func _physics_process(delta: float) -> void:
 	time += delta
 	light.energy = 1.16 + 0.2 * sin(4 * time)
 	
-	var direction: Vector2 = Input.get_vector("KEY_A", "KEY_D", "KEY_W", "KEY_S")
-	
-	if direction:
-		velocity = direction * movement_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, movement_speed/4)
-		velocity.y = move_toward(velocity.y, 0, movement_speed/4)
-	
 	move_and_slide()
 
 func camera_translation(cell_pos: Vector2i, duration: float) -> void:
 	var actual_pos = Vector2(cell_pos) * get_viewport_rect().size
+
 	var tween_1 = create_tween()
-	var tween_2 = create_tween()
-	var tween_3 = create_tween()
-	var tween_4 = create_tween()
 	tween_1.tween_property(camera, "limit_left", actual_pos.x, duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	var tween_2 = create_tween()
 	tween_2.tween_property(camera, "limit_right", actual_pos.x, duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	var tween_3 = create_tween()
 	tween_3.tween_property(camera, "limit_top", actual_pos.y, duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	var tween_4 = create_tween()	
 	tween_4.tween_property(camera, "limit_bottom", actual_pos.y, duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
 
 func take_damage(damage: int) -> void:
